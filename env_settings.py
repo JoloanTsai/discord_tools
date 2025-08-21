7954fac5815ed5b6cff10ad0d4a58ad6190dfe7b
Joloan <joeopen0@gmail.com>
2025-08-21 21:33:57 +0800

    first commit

---
+++ env_settings.py
@@ -0,0 +1,22 @@
+from collections.abc import Iterable
+import os
+
+# Save Path
+ATTACHMENT_FOLD = 'atacchments'
+CHAT_FOLD = 'chat_history'
+SERVER_INFO_FILE_PATH = 'server_info.json'
+MESSAGES_LIMIT = 3000
+CHANNEL_TYPE : Iterable|str|None = 'text' # 篩選想要儲存的頻道類型
+
+# 可調
+GUILD_IDS : Iterable|int|str|None = None  # 輸入指定的伺服器 ID(s)，只會收集這些伺服器的頻道，None 為全部
+
+
+
+
+def get_discord_token():
+    t = os.environ.get('DISCORD_BOT_TOKEN')
+    if t is not None: return t
+    else : raise ValueError("DISCORD_BOT_TOKEN 環境變量未設置")
+
+DISCORD_BOT_TOKEN = get_discord_token()
